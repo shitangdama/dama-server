be4dbdd9cb3d92592b472bbbb434d8d500659200
shitangdama <kbr1990117@gmail.com>
2017-07-04 11:04:46 +0800

    websocket

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var log4js = require('log4js');
+const log4js = require('log4js');
+const WebSocket = require('ws');
 var logger = log4js.getLogger();
 // logger.level = 'debug';
 
-setInterval(function(){
-logger.debug("Some debug messages");
-},1000)
+// setInterval(function(){
+//   console.log(111111111111)
+//   console.log("111111111111")
+//   console.log("asdasdasd")
+//   console.log("测试")
+//   logger.debug("Some debug messages");
+// },1000)
+
+const = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+      console.log(111111111111)
+      console.log("111111111111")
+      console.log("asdasdasd")
+      console.log("测试")
+      logger.debug("Some debug messages");
+      ws.send('something');
+  });
+});
