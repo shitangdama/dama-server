5c8b07a66ecb1f68ce3ba4646af9701d81688193
shitangdama <kbr1990117@gmail.com>
2017-07-05 15:58:56 +0800

    websocket

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const log4js = require('log4js');
 const WebSocket = require('ws');
 var logger = log4js.getLogger();
 
-const = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ port: 3000 });
 
-
-//
-// 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
